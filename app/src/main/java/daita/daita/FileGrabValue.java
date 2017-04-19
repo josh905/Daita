adc7dae1e4c557b899ff8dd113962c2b8255212a
josh905 <joshpatrickreynolds@gmail.com>
2017-04-19 17:21:32 +0100

    final changes, code shortening, removing excess imports 2

--- app/src/main/java/daita/daita/FileGrabValue.java
+++ app/src/main/java/daita/daita/FileGrabValue.java
@@ -19,31 +19,6 @@ import java.util.ArrayList;
 
 public class FileGrabValue {
 
-    private Context con;
-
-    public String getValue(Context con, int file){
-
-        InputStream input = con.getResources().openRawResource(file);
-        BufferedReader buff = new BufferedReader(new InputStreamReader(input));
-
-        String row;
-        String[] col;
-        String theValue = "none";
-        try {
-            while ((row = buff.readLine()) != null) {
-
-                col = row.split(",");
-                theValue = col[1];
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return theValue;
-
-    }
-
-
 
 
 
